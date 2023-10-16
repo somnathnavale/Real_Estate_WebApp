48bd0d9434f45b7686c3bfc4a69df2d326f41e0a
somnath navale <somnathnavale112001@gmail.com>
2023-10-16 17:18:31 +0530

    added middleware for error and added signup route

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import bcryptjs from 'bcryptjs';
+import User from "../models/user.model.js";
+
+const signup=async(req,res,next)=>{
+    const {username,email,password}=req.body;
+    const hashedPassword=bcryptjs.hashSync(password,12);
+    const newUser=new User({username,email,password:hashedPassword});
+    try {
+        await newUser.save();
+        res.status(201).json({message:"User Created Successfully"});
+    } catch (error) {
+        next(error);
+    }
+} 
+
+
+export {signup};
--- server/server.js
+++ server/server.js
@@ -1,15 +1,32 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
+
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log("connected to mongogdb");
+    console.log("connected to mongodb");
 }).catch(err=>{
     console.log(err);
 });
 
 const app=express();
+app.use(express.json());
+
+app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success:false,
+        statusCode, 
+        message
+    });
+})
 
 app.listen(3000,()=>{
     console.log("server is running...");
