35ab566063b668ddec14567fee5bbac34913d867
somnath navale <somnathnavale112001@gmail.com>
2023-10-16 15:50:59 +0530

    added database connection.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("connected to mongogdb");
+}).catch(err=>{
+    console.log(err);
+});
 
 const app=express();
 
