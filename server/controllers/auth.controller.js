ec464235bced53e8ef9cad9ba08cb11994e51fdd
somnath navale <somnathnavale112001@gmail.com>
2023-10-17 20:05:34 +0530

    added sign in

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from "../models/user.model.js";
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
@@ -13,5 +15,28 @@ const signup=async(req,res,next)=>{
     }
 } 
 
+const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    try {
+        if(!email || !password){
+            throw errorHandler(400,"Both the fields are required");
+        }
+        const validUser=await User.findOne({email:email});
+        if(!validUser){
+            throw errorHandler(404,"User not found");
+        }
+        const validPassword=await bcryptjs.compare(password,validUser.password);
+        if(!validPassword)
+            throw errorHandler(401, "Invalid Credentials")
+        const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
 
-export {signup};
+        const {password:pass,...userInfo}=validUser._doc;
+        res.cookie('access_token',token,{httpOnly:true});
+        res.status(200).json({user:userInfo});
+    } catch (error) {
+        next(error);
+    }
+}
+
+export {signup,signin};
+
