89c89ec74096d09413cc3feb9fdd81030d9ac91a
somnath navale <somnathnavale112001@gmail.com>
2023-12-06 17:49:46 +0530

    used module for path

--- server/utils/constants/emailConstants.js
+++ server/utils/constants/emailConstants.js
@@ -1,16 +1,23 @@
 import fs from "fs";
+import {resolve, dirname} from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const forgotPasswordMailContent={
     subject:"Password Reset",
-    html:fs.readFileSync('./utils/view/forgot-password-template.html', 'utf-8'),
+    html:fs.readFileSync(resolve(__dirname,'../' ,'view', 'forgot-password-template.html'), 'utf-8'),
     from:"",
     to:"" 
 }
 
 export const UserRegistrationMailContent={
     subject:"Welcome to NextGen Estate",
-    html:fs.readFileSync('./utils/view/user-registration-template.html', 'utf-8'),
+    html:fs.readFileSync(resolve(__dirname,'../' ,'view', 'user-registration-template.html'), 'utf-8'),
     from:"",
     to:"" 
 }
+
+console.log(forgotPasswordMailContent);
 
