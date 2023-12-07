d7f6531fdfa6be637ed286019571a3cad21b9899
somnath navale <somnathnavale112001@gmail.com>
2023-12-07 10:21:41 +0530

    updated z-index of information modal

--- client/src/components/InformationModal.jsx
+++ client/src/components/InformationModal.jsx
@@ -5,7 +5,7 @@ const InformationModal = ({ owner, open, onClose }) => {
   return (
     <>
       {open && (
-        <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="fixed z-20 inset-0 overflow-y-auto">
           <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <div
               className="fixed inset-0 transition-opacity"
